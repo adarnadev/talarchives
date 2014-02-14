8e6b13551d54551ef91bcf1024e3b72fe71c0f4d
Agno Almario <agnoalmario@Agnos-MacBook-Pro.local>
2014-02-14 15:31:11 +0800

    new stuff

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,12 +1,13 @@
+
 (function(){
-    $("body").hide().fadeIn(2000);
+    $("body").hide().fadeIn(4000);
 
 })();
 
 function resizeContent() {
 	var f =	$(".frame")
 	
-	f.css("height", window.innerHeight - 180 - 36 -18)
+	f.css("height", window.innerHeight - 180 - 36 -18 - 10)
 }
 
 $(document).ready(function() {
@@ -15,4 +16,14 @@ $(document).ready(function() {
 	$(window).resize(function(event) {
 		resizeContent()
     })
+
+	$(".navbutton").css("opacity",1.0)
+	$(".navbutton").hover(function() {
+		$(this).animate({opacity:0.5},1000)	
+	}, function() {
+		$(this).animate({opacity:1.0},500)
+	})
+
+
+
 })
